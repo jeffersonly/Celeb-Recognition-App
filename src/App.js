8dc81e6b236904f7cdaed1e605d4a154eb325b2a
jeffersonly <xjlsays@gmail.com>
2019-11-18 12:03:09 -0800

    forgot password and reset password ui and functionalities added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './Pages/Auth/Login';
 import Register from './Pages/Auth/Register';
 import Home from './Pages/Auth/Home';
 import Confirm from './Pages/Auth/ConfirmCode';
+import ForgotPassword from './Pages/Auth/ForgotPassword';
+import ChangePassword from './Pages/Auth/ChangePassword';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -17,6 +19,8 @@ function App() {
           <Route path="/Home" component={Home} />
           <Route path="/Register" component={Register} />
           <Route path="/Confirm" component={Confirm} />
+          <Route path="/ForgotPassword" component={ForgotPassword} />
+          <Route path="/ChangePassword" component={ChangePassword} />
         </Switch>
       </Router>
     </div>
