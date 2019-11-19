d90727a8c7c90e104675d8179a5cf6805767f03c
jeffersonly <xjlsays@gmail.com>
2019-11-18 16:50:47 -0800

    boosted route protection for home page

--- src/Pages/Auth/Home.js
+++ src/Pages/Auth/Home.js
@@ -37,15 +37,21 @@ function Home(props) {
     .catch(err => console.log(err));
   }
 
+  //check if user is authenticated/logged in
+  function isAuthenticated() {
+    Auth.currentAuthenticatedUser()
+    .then(user => console.log(user))
+    .catch(err => 
+      window.location.href = "/Login",
+      window.alert("Please log in to view page")
+    );
+  }
+  {isAuthenticated();}
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
         <button onClick={checkUser}>Check User</button>
         <button onClick={signOut}>Sign Out</button>
-        <button onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}>Sign In with Facebook</button>
-        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign In with Google</button>
-
         <input type="file" onChange={identifyFile}></input>
       </header>
     </div>
