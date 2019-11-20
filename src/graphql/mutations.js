683d2c714dd0aadbfbdbf3a18a17e24d1d0e788d
jeffersonly <xjlsays@gmail.com>
2019-11-19 23:02:39 -0800

    added amplify api (graphql) and storage (s3)

---
+++ src/graphql/mutations.js
@@ -0,0 +1,183 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = `mutation CreateUser($input: CreateUserInput!) {
+  createUser(input: $input) {
+    id
+    username
+    avatar {
+      bucket
+      region
+      key
+    }
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    username
+    avatar {
+      bucket
+      region
+      key
+    }
+  }
+}
+`;
+export const deleteUser = `mutation DeleteUser($input: DeleteUserInput!) {
+  deleteUser(input: $input) {
+    id
+    username
+    avatar {
+      bucket
+      region
+      key
+    }
+  }
+}
+`;
+export const createPost = `mutation CreatePost($input: CreatePostInput!) {
+  createPost(input: $input) {
+    id
+    title
+    description
+    avatar {
+      bucket
+      region
+      key
+    }
+    filename
+    key
+    userid
+    comments {
+      items {
+        id
+        author
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const updatePost = `mutation UpdatePost($input: UpdatePostInput!) {
+  updatePost(input: $input) {
+    id
+    title
+    description
+    avatar {
+      bucket
+      region
+      key
+    }
+    filename
+    key
+    userid
+    comments {
+      items {
+        id
+        author
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const deletePost = `mutation DeletePost($input: DeletePostInput!) {
+  deletePost(input: $input) {
+    id
+    title
+    description
+    avatar {
+      bucket
+      region
+      key
+    }
+    filename
+    key
+    userid
+    comments {
+      items {
+        id
+        author
+        content
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const createComment = `mutation CreateComment($input: CreateCommentInput!) {
+  createComment(input: $input) {
+    id
+    author
+    content
+    post {
+      id
+      title
+      description
+      avatar {
+        bucket
+        region
+        key
+      }
+      filename
+      key
+      userid
+      comments {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateComment = `mutation UpdateComment($input: UpdateCommentInput!) {
+  updateComment(input: $input) {
+    id
+    author
+    content
+    post {
+      id
+      title
+      description
+      avatar {
+        bucket
+        region
+        key
+      }
+      filename
+      key
+      userid
+      comments {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteComment = `mutation DeleteComment($input: DeleteCommentInput!) {
+  deleteComment(input: $input) {
+    id
+    author
+    content
+    post {
+      id
+      title
+      description
+      avatar {
+        bucket
+        region
+        key
+      }
+      filename
+      key
+      userid
+      comments {
+        nextToken
+      }
+    }
+  }
+}
+`;
